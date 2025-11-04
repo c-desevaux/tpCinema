8903de924c988a4f6280469852a95cac4f2e53ef
cdesevaux <corentin.desevaux@gmail.com>
2025-11-04 16:21:12 +0100

    update portrait img

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
     const grade = document.getElementById("grade");
     const genre  = document.getElementById("genre");
     const filmMaker = document.getElementById("film-maker");
-    const actors = document.getElementById("actors");
+    const actors = document.getElementById("actors-container");
     const synopsis = document.getElementById("synopsis");
     
     let actorsTab = [];
@@ -30,30 +30,36 @@
 
             
             for(let i=0 ; i<nbActor ; i++){
-                let a = document.createElement("p");
+                let actor = document.createElement("p");
                 if(i===0){
-                    a.textContent = data.cast[i].name+", ";
-                    a.style.marginLeft = "5px";
+                    actor.textContent = data.cast[i].name+", ";
+                    actor.style.marginLeft = "5px";
                 }
                 else if(i===nbActor-1){
-                    a.textContent = data.cast[i].name
+                    actor.textContent = data.cast[i].name
                 }else{
-                    a.textContent = data.cast[i].name+", ";
+                    actor.textContent = data.cast[i].name+", ";
                 }
                 
-                a.id = `actor${i}`;
-                a.style.paddingRight = "3px";
-                a.style.paddingLeft = "3px";
-                a.style.cursor = "pointer";
-                a.addEventListener("mouseover", ()=>{
-                    let b = document.createElement("img");
-                    b.src = "https://image.tmdb.org/t/p/original/"+data.cast[i].profile_path;
-                    b.style.width = "60px";
-                    b.style.borderRadius = "10px";
-                    a.innerHTML="";
-                    a.appendChild(b);
+                actor.id = `actor${i}`;
+                actor.style.paddingRight = "3px";
+                actor.style.paddingLeft = "3px";
+                actor.style.cursor = "pointer";
+                let actorImg = document.createElement("img");
+                let actorText = actor.textContent;
+                actorImg.className = "actor-img";
+                actor.addEventListener("mouseover", ()=>{
+                    
+                    actorImg.src = "https://image.tmdb.org/t/p/original/"+data.cast[i].profile_path;
+                    actor.appendChild(actorImg);
                 })
-                actors.appendChild(a);
+                
+                actor.addEventListener("mouseleave", () => {
+                   
+                    actor.innerHTML = "";
+                    actor.textContent = actorText;
+                })
+                actors.appendChild(actor);
             }
 
         });
