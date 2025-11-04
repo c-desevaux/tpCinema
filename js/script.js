df2f81a731eed33229443571663db2dd1cd38796
cdesevaux <corentin.desevaux@gmail.com>
2025-11-04 11:50:32 +0100

    ajout fichier json

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 "use strict";
 
+const movieImg = document.getElementById("movie-img");
+const title = document.getElementById("title");
+const date = document.getElementById("date");
+const grade = document.getElementById("grade");
+const genre  = document.getElementById("genre");
+const filmMaker = document.getElementById("film-maker");
+const actors = document.getElementById("actors");
+const synopsis = document.getElementById("synopsis");
+
+
