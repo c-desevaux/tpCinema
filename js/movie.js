cbdd2715753a0750e5a6137aa578a0d64af4b732
cdesevaux <corentin.desevaux@gmail.com>
2025-11-05 16:08:50 +0100

    fix no profile image

--- js/movie.js
+++ js/movie.js
@@ -137,7 +137,7 @@ fetch(urlCredits)
                 actor.addEventListener("mouseover", () => {
                     if (data.cast[i].profile_path) {
                         actorImg.src = "https://image.tmdb.org/t/p/original/" + data.cast[i].profile_path;
-                    } else { actorImg.src = "../ressources/unkown_person.jpg"; }
+                    } else { actorImg.src = "../ressources/unknown_person.jpg"; }
 
                     container.appendChild(actorImg);
                     container.appendChild(character);
